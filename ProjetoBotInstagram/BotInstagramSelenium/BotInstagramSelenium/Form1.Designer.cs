8a3680c860530359e0c781b68d517241041e08dd
Izabela Campos <bebelacampostcms@gmail.com>
2021-05-19 10:57:53 -0300

    subindo primeira versao

---
+++ ProjetoBotInstagram/BotInstagramSelenium/BotInstagramSelenium/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace BotInstagramSelenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.Transparent;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_login.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_login.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_login.Location = new System.Drawing.Point(213, 308);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(134, 58);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(33, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(33, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Senha:";
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(179, 177);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(261, 27);
+            this.txt_usuario.TabIndex = 3;
+            this.txt_usuario.TextChanged += new System.EventHandler(this.TestNullTxtBox);
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Location = new System.Drawing.Point(179, 219);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.Size = new System.Drawing.Size(261, 27);
+            this.txt_senha.TabIndex = 4;
+            this.txt_senha.UseSystemPasswordChar = true;
+            this.txt_senha.TextChanged += new System.EventHandler(this.TestNullTxtBox);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BotInstagramSelenium.Properties.Resources.instagram_log;
+            this.pictureBox1.Location = new System.Drawing.Point(213, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 391);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_login);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ProjetoBotInstagram/BotInstagramSelenium/BotInstagramSelenium/Form1.cs
@@ -0,0 +1,114 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BotInstagramSelenium
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        ChromeDriver driver; Thread th;
+        string url = "https://www.instagram.com/";
+        public string usuario;
+        public string senha;
+
+        private void TestNullTxtBox(object sender, EventArgs e)
+        {
+            usuario = txt_usuario.Text;
+            senha = txt_senha.Text;
+
+            if (!string.IsNullOrEmpty(usuario) && !string.IsNullOrEmpty(senha))
+                btn_login.ForeColor = Color.Lime;
+            else
+                btn_login.ForeColor = Color.Red;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (btn_login.ForeColor == Color.Lime)
+            {
+                th = new Thread(Login_Insta);
+                th.Start();
+            }
+        }
+
+        private void Login_Insta()
+        {
+            Thread.Sleep(3000);
+            try
+            {
+                //Campo usuário
+                driver.FindElement(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']")).SendKeys(usuario);
+                Thread.Sleep(3000);
+
+                //Campo senha
+                driver.FindElement(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']")).SendKeys(senha);
+                Thread.Sleep(3000);
+
+                //botão login
+                driver.FindElement(By.XPath("//button[@class = 'sqdOP  L3NKy   y3zKF     ']")).Click();
+                Thread.Sleep(5000);
+
+                string erroSenha = driver.FindElementByClassName("eiCW-")?.Text;
+                if (erroSenha != null)
+                {
+                    MessageBox.Show("Senha incorreta!");
+                    driver.Close();
+                    Application.Exit();
+                }
+                else
+                    ValidacaoPosLogin();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex);
+            }
+        }
+
+        private void ValidacaoPosLogin()
+        {
+            driver.FindElement(By.XPath("//button[@class = 'sqdOP yWX7d    y3zKF     ']")).Click();
+            Thread.Sleep(6000);
+
+            driver.FindElement(By.XPath("//button[@class = 'aOOlW   HoLwm ']")).Click();
+            Thread.Sleep(2000);
+
+            // Ir para a pagina solicitada
+            //try
+            //{
+            //    driver.Navigate().GoToUrl(linkPost); Thread.Sleep(2000);
+            //    // Método sem retorno para escolha aleatória do comentário a ser digitado
+            //    comentarios();
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            driver.Quit();
+        }
+    }
+}
